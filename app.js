12fa515ef0291430286352c9a950b041d0308459
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-12 14:53:26 -0400

    can now enter messages

--- app.js
+++ app.js
@@ -15,16 +15,39 @@ const app = {
             .querySelector(selectors.formSelector)
             .addEventListener('submit', this.sendMsg.bind(this))
 
-        this.serversLoad()
+        document
+            .querySelector(selectors.svrSelector)
+            .addEventListener('submit', this.addServer.bind(this))
 
+        this.serversLoad()
     },
 
     help() {
         alert('You can join a server and send messages')
     },
 
-    serversLoad() {
+    addServer(ev) {
+        ev.preventDefault()
 
+        const f = ev.target
+
+        const server = {
+            id: this.max,
+            name: f.message.value
+        }
+
+        console.log('server : ' + server)
+
+        f.reset()
+
+        this.serversLoad()
+    },
+
+    serversLoad() {
+        for (let i = 0; i < svrList.length; i++) {
+            const item = document.createElement('li')
+            item.textContent = svrList[i].name
+        }
     },
 
     messagesLoad() {
@@ -34,12 +57,19 @@ const app = {
     sendMsg(ev) {
         ev.preventDefault()
 
+        const f = ev.target
+        const msg = document.createElement('li')
+        msg.textContent = f.message.value
 
+        const list = document.querySelector('#msgList')
+        console.log(msg)
+        list.appendChild(msg)
+
+        f.reset()
     },
 }
 
 app.init({
     formSelector: '#msgForm',
-    msgSelector: '#msgList',
     svrSelector: '#svrList',
 })
